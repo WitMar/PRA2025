e5f42a68d6be0cadd0bb74dc765e02e623687ed5
marcin witkowski <marcin.witkowski@e-qsg.com>
2025-03-02 22:47:10 +0100

    fix errors

--- src/main/java/second/debug/hidden/ObjectMaker.java
+++ src/main/java/second/debug/hidden/ObjectMaker.java
@@ -20,6 +20,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import static java.lang.Double.valueOf;
+
 /**
  * STOP LOOKING AT THIS CLASS!! IT'S OFF LIMITS!
  *
@@ -33,7 +35,7 @@ public class ObjectMaker {
         final List<Integer> list = new ArrayList<Integer>();
 
         for (int i = 0; i <= size - 1; i++) {
-            list.add(new Double(Math.random() * 100000).intValue());
+            list.add(valueOf(Math.random() * 100000).intValue());
         }
         list.add(null);
 
@@ -44,7 +46,7 @@ public class ObjectMaker {
         final Object[] array = new Object[size];
 
         for (int i = 0; i <= size - 1; i++) {
-            array[i] = new Double(Math.random() * 100000);
+            array[i] = Double.valueOf(Math.random() * 100000);
         }
 
         //array[(int) (Math.random() * (size - 1))] = null;
