778c737269f666bac0160c6c4d73300b952fbfe3
marcin witkowski <marcin.witkowski@e-qsg.com>
2025-04-15 19:20:40 +0200

    Fix test

--- src/test/java/com/pracownia/spring/WebAppApplicationTests.java
+++ src/test/java/com/pracownia/spring/WebAppApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = SpringAppConfig.class)
 public class WebAppApplicationTests {
 
     @Test
