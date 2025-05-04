f3fec97ab73151c7556e0aea0414400e34b7adbf
marcin witkowski <marcin.witkowski@e-qsg.com>
2025-05-04 14:45:15 +0200

    class final code

--- src/TodoList/TodoList.js
+++ src/TodoList/TodoList.js
@@ -1,31 +1,114 @@
 import React from 'react';
-import styled from 'styled-components';
 import Todo from "../Todo/Todo";
-
-const greenStyle = {
-    color: 'green',
-};
-
-const Undone = styled.li`
-  color: red;
-`;
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class TodoList extends React.Component {
+    constructor(props) {
+        super(props);
+        let t1 = { id : 0, value : "Learn React", done : true };
+        let t2 = { id : 1, value : "Pass Programming Laboratory", done : false };
+        let t3 = { id : 2, value : "Have a nice holiday", done : false };
+        let newTodo = "";
+        this.state = { todosList: [t1, t2, t3], newTodo };
+        this.addTodo = this.addTodo.bind(this);
+        this.counter = 2;
+    };
+
+    componentDidMount() {
+        axios.get(`http://localhost:8080/todos`)
+            .then(res => {
+                const todosList = res.data;
+                this.setState({ todosList });
+            })
+    }
+
+    addTodo = () => {
+        let nextId = this.state.todosList.map(a => { return a.id }).reduce((acc, val) => {
+            return acc > val ? acc : val;
+        });
+        nextId++;
+        let newelement = {id: nextId, value: this.state.newTodo, done: false};
+        this.setState(prevState => ({
+            todosList: [...prevState.todosList, newelement],
+            newTodo: ""
+        }), () => axios.post(`http://localhost:8080/todos/`, newelement));
+    };
+
+    handleRemove = id => {
+        let object= this.state.todosList.find(function (item) {
+            if(item.id === id) return item;
+            return null;
+        });
+        let arrAfterDel = this.state.todosList.filter(function (item) {
+            return item.id !== id
+        });
+        this.setState({todosList: arrAfterDel}, () => axios.delete(`http://localhost:8080/todos/${object.id}`, object));
+    }
+
+    updateChild = (id, done) => {
+        let arrAfterUpdate = this.state.todosList.map(function (item) {
+            if(item.id === id) item.done = done;
+            return item;
+        });
+
+        let object = arrAfterUpdate.find(function (item) {
+            if(item.id === id) return item;
+            return null;
+        });
+
+        this.setState({todosList: arrAfterUpdate}, () => axios.put(`http://localhost:8080/todos/${object.id}`, object));
+
+        console.log(arrAfterUpdate);
+    };
+
+    myChangeHandler = (event) => {
+        this.setState({newTodo: event.target.value});
+    }
+
     render() {
+
+        const {todosList} = this.state;
+
+        let todos = todosList.map(todo => {
+            return (<tr key={todo.id}>
+                <Todo value={todo.value}
+                      done={todo.done}
+                      id={todo.id}
+                      update={(id, done) => this.updateChild(id, done)}  />
+                <th>
+                    <button type="button" onClick={() => this.handleRemove(todo.id)}>
+                        Remove
+                    </button>
+                </th>
+            </tr>);
+        })
+
         return (
             <div className="TodoList">
                 <h1>Todo List {this.props.name}</h1>
-                <ul>
-                    <li style={greenStyle}>Learn React</li>
-                    <Undone>Pass Programming Laboratoty</Undone>
-                    <Todo value="Have a nice holiday"
-                          done={false}   />
-                </ul>
+                <table className="table table-striped">
+                    <thead className="thead-dark">
+                    <th scope="col">Todo</th>
+                    <th scope="col">Status</th>
+                    <th scope="col"></th>
+                    </thead>
+                    <tbody>
+                    {todos}
+                    </tbody>
+                </table>
+                <p> My new todo </p>
+                <input
+                    type='text'
+                    onChange={this.myChangeHandler}
+                    value={this.state.newTodo}
+                />
+                <button onClick={this.addTodo}>
+                    AddTodo
+                </button>
             </div>
         );
     }
 }
 
 export default TodoList;
-
-
