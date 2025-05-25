e958804947126a418362c4605cff2b0f87ae3c9c
marcin witkowski <marcin.witkowski@e-qsg.com>
2025-05-25 12:32:22 +0200

    React Spring Security

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {ThemeProvider} from "./ContextExample/ThemeProvider";
+import ReactDOM from 'react-dom';
+import App from './app/App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <ThemeProvider>
-        <p class="indented"><App/></p>
-    </ThemeProvider>
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>, 
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
